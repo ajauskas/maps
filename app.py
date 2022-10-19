005e21ff5eac88fc6ebdf47e780344306b752fd7
Rodrigo Ajauskas <rodrigo_ajauskas@hotmail.com>
2022-10-19 19:17:14 +0100

    Update app.py

--- app.py
+++ app.py
@@ -1,10 +1,16 @@
-from flask import Flask, render_template
+from flask import Flask
+
+import folium
 
 app = Flask(__name__)
 
+
 @app.route('/')
-def render_the_map():
-    return render_template('RodoanelSP_Features20221910.html')
+def index():
+    start_coords = (46.9540700, 142.7360300)
+    folium_map = folium.Map(location=start_coords, zoom_start=14)
+    return folium_map._repr_html_()
+
 
 if __name__ == '__main__':
     app.run(debug=True)
